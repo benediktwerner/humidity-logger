ad3ed92c39de19888ffa29c00d934179d24242b5
Gaurav Pathak <gauravpathak129@gmail.com>
2023-01-29 02:11:09 +0530

    Added Grafans setup instructions in setup script
    Signed-off-by: Gaurav Pathak <gauravpathak129@gmail.com>

--- setup-data-node.sh
+++ setup-data-node.sh
@@ -11,6 +11,9 @@ DEFAULT_USER="wernerfamily"
 SCRIPT_URL="https://raw.githubusercontent.com/gauravpathak/humidity-logger/master/logger.py"
 SERVICE_URL="https://raw.githubusercontent.com/gauravpathak/humidity-logger/master/humidity-logger.service"
 CONFIG_URL="https://raw.githubusercontent.com/gauravpathak/humidity-logger/master/config.toml.example"
+
+GRAFANA_DATA_SRC_FILE="influxdb-datasource.yaml"
+GRAFANA_DATA_SRC_URL="https://raw.githubusercontent.com/gauravpathak/humidity-logger/setup-script/influxdb-datasource.yaml.example"
 CONFIG_FILE="${CONFIG_URL##*/}"
 SAMPLING_PERIOD=5 #seconds
 INFLUX_TOKEN=""
@@ -86,8 +89,34 @@ sed -i "s/%influxport%/${INFLUX_PORT}/g" "${HOME_DIR}/humidity-logger/${CONFIG_F
 sed -i "s/%influxorg%/${INFLUX_ORG}/g" "${HOME_DIR}/humidity-logger/${CONFIG_FILE%.*}"
 sed -i "s/%influxbucket%/${INFLUX_BUCKET}/g" "${HOME_DIR}/humidity-logger/${CONFIG_FILE%.*}"
 
+# Grafana Setup 
+GRAFANA_PKG="grafana"
+GRAFANA_KEY_STATUS=$(apt-key list 2> /dev/null | grep "${GRAFANA_PKG}")
+GRAFANA_INSTALL_STATUS=$(dpkg-query --status "${GRAFANA_PKG}" | grep Status)
+if [[ ! $GRAFANA_KEY_STATUS ]]; then
+    wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+    echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+    apt update
+fi
+
+if [[ ! "$GRAFANA_INSTALL_STATUS" == *"install ok installed"* ]]; then
+    apt install -y "${GRAFANA_PKG}"
+    systemctl daemon-reload
+    systemctl enable grafana-server
+    systemctl start grafana-server
+fi
+
+${WGET} -4 "${GRAFANA_DATA_SRC_URL}" -O "${HOME_DIR}/humidity-logger/${GRAFANA_DATA_SRC_FILE}"
+sed -i "s/%influxtoken%/${INFLUX_TOKEN}/g" "${HOME_DIR}/humidity-logger/${GRAFANA_DATA_SRC_FILE}"
+sed -i "s/%influxhost%/${INFLUX_HOST}/g" "${HOME_DIR}/humidity-logger/${GRAFANA_DATA_SRC_FILE}"
+sed -i "s/%influxport%/${INFLUX_PORT}/g" "${HOME_DIR}/humidity-logger/${GRAFANA_DATA_SRC_FILE}"
+sed -i "s/%influxorg%/${INFLUX_ORG}/g" "${HOME_DIR}/humidity-logger/${GRAFANA_DATA_SRC_FILE}"
+sed -i "s/%influxbucket%/${INFLUX_BUCKET}/g" "${HOME_DIR}/humidity-logger/${GRAFANA_DATA_SRC_FILE}"
+mv "${HOME_DIR}/humidity-logger/${GRAFANA_DATA_SRC_FILE}" "/etc/grafana/provisioning/datasources/"
+
+systemctl stop grafana-server
+systemctl start grafana-server
+
 systemctl enable humidity-logger
 systemctl stop humidity-logger
 systemctl start humidity-logger
-
-
